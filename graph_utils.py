3272fa6be8fdaba0df4dca2376828f1ef9de63da
Ryan Mccarlie <ryanmccarlie@Ryans-MacBook-Air.local>
2023-02-05 18:24:22 +0200

    chore: Refactor code to graph and UI utils

--- graph_utils.py
+++ graph_utils.py
@@ -1,26 +1,78 @@
-from constants import TOTAL_COLS, TOTAL_ROWS, TILE_SIZE
+from typing import List, Dict
+
+from constants import TOTAL_COLS, TOTAL_ROWS, TILE_SIZE, GRID_COSTS
+
+import pygame as pg
 
 
-def get_circle_coords(x, y):
-    return (x * TILE_SIZE + TILE_SIZE // 2, y * TILE_SIZE + TILE_SIZE // 2), TILE_SIZE // 4
+def get_shortest_path_from_visited_graph(node_came_from_node: dict, start_node: tuple[int, int],
+                                         goal_node: tuple[int, int]) -> list[int]:
+    current_node = node_came_from_node.get(goal_node)
+    shortest_path = [goal_node]
+
+    while current_node != start_node:
+        shortest_path.append(current_node)
+        current_node = node_came_from_node[current_node]
+
+    shortest_path_reversed = shortest_path + [start_node]
+    shortest_path = list(reversed(shortest_path_reversed))
+    return shortest_path
 
 
-def get_rectangular_coords(x: int, y: int):
-    return x * TILE_SIZE + 1, y * TILE_SIZE + 1, TILE_SIZE - 2, TILE_SIZE - 2
+def init_structure_components(grid_costs: List = GRID_COSTS, total_columns: int = TOTAL_COLS,
+                              total_rows: int = TOTAL_ROWS, tile_size: float = TILE_SIZE
+                              ) -> tuple[pg.Surface, List, Dict]:
+    pg.init()
+    screen = pg.display.set_mode(size=[total_columns * tile_size, total_rows * tile_size])
 
+    grid = [
+        [int(char) for char in string]
+        for string in grid_costs
+    ]
+    _grid = grid.copy()
 
-def get_next_nodes(grid: list, x: int, y: int):
-    check_next_node = lambda x, y: True if 0 <= x < TOTAL_COLS and 0 <= y < TOTAL_ROWS and not grid[y][x] else False
-    ways = [-1, 0], [0, -1], [1, 0], [0, 1]
-    return [(x + dx, y + dy) for dx, dy in ways if check_next_node(x + dx, y + dy)]
-
-
-def init_graph(grid: list):
     # dict of adjacency lists
     graph = {}
-    for y, row in enumerate(grid):
+    for y, row in enumerate(_grid):
         for x, col in enumerate(row):
-            if not col:
-                graph[(x, y)] = graph.get((x, y), []) + get_next_nodes(grid, x, y)
+            graph[(x, y)] = graph.get((x, y), []) + get_valid_adjacent_nodes_with_weights(grid, x, y)
 
-    return graph
+    return screen, grid, graph
+
+
+def get_circle_coords(x: int, y: int, tile_size: int = TILE_SIZE):
+    return (x * tile_size + tile_size // 2, y * tile_size + tile_size // 2), tile_size // 4
+
+
+def get_rectangular_coords(x: int, y: int, tile_size: int = TILE_SIZE):
+    return x * tile_size + 1, y * tile_size + 1, tile_size - 2, tile_size - 2
+
+
+def get_valid_adjacent_nodes_with_weights(grid: list, x: int, y: int, total_cols: int = TOTAL_COLS,
+                                          total_rows: int = TOTAL_ROWS) -> List:
+    def _check_next_node(_x, _y):
+        if 0 <= _x < total_cols and 0 <= _y < total_rows:
+            return True
+        return False
+
+
+    ways = [-1, 0], [0, -1], [1, 0], [0, 1]
+    adjacent_nodes = [
+        (grid[y + dy][x + dx], (x + dx, y + dy)) for dx, dy in ways
+        if _check_next_node(x + dx, y + dy)
+    ]
+
+    return adjacent_nodes
+
+
+def get_next_nodes_without_weight(grid: list, x: int, y: int, total_cols: int = TOTAL_COLS,
+                                  total_rows: int = TOTAL_ROWS) -> List:
+    def _check_next_node(_x, _y):
+        if 0 <= _x < total_cols and 0 <= _y < total_rows and not grid[y][x]:
+            return True
+        return False
+
+    ways = [-1, 0], [0, -1], [1, 0], [0, 1]
+    return [(x + dx, y + dy) for dx, dy in ways if _check_next_node(x + dx, y + dy)]
+
+
--- ui_utils.py
+++ ui_utils.py
@@ -1,15 +1,53 @@
 import pygame as pg
 
+from constants import TILE_SIZE, TOTAL_ROWS, TOTAL_COLS
+from graph_utils import get_circle_coords
 
-def update_screen_path_cost(screen, path_cost=0):
+
+def draw_circle_nodes(screen: pg.Surface, visited, color: pg.Color):
+    for x, y in visited:
+        center, radius = get_circle_coords(x, y)
+        pg.draw.circle(screen, color=color, center=center, radius=radius)
+
+
+def update_screen_with_all_node(screen: pg.Surface, start_node: tuple[int, int], goal_node: tuple[int, int]):
+    pg.draw.circle(screen, pg.Color('black'), *get_circle_coords(*start_node, tile_size=TILE_SIZE))
+    pg.draw.circle(screen, pg.Color('black'), *get_circle_coords(*goal_node, tile_size=TILE_SIZE))
+
+
+def get_click_mouse_pos():
+    x, y = pg.mouse.get_pos()
+    grid_x, grid_y = x // TILE_SIZE, y // TILE_SIZE
+    click = pg.mouse.get_pressed()
+
+    if click[0]:
+        return grid_x, grid_y
+
+    return False
+
+
+def set_screen_with_image(screen: pg.Surface, show_grid: bool):
+    background_img = "images/img_without_grid.png"
+    if show_grid:
+        background_img = "images/img_with_grid.png"
+
+    # Add background image
+    background = pg.image.load(background_img).convert()
+    background = pg.transform.scale(background, (TOTAL_COLS * TILE_SIZE, TOTAL_ROWS * TILE_SIZE))
+
+    # fill screen
+    screen.blit(background, (0, 0))
+
+
+def update_screen_path_cost(screen: pg.Surface, path_cost: int = 0):
     text = f"Path Cost: {path_cost}"
-    pg_large_font = pg.font.SysFont('comicsans', 30)  # Font object
+    pg_large_font = pg.font.SysFont("comicsans", 30)  # Font object
 
     text = pg_large_font.render(text, True, (0, 0, 0))  # create our text
     screen.blit(text, (10, 3))  # draw the text to the screen
 
 
-def update_screen_execution_time(screen, execution_time):
+def update_screen_execution_time(screen: pg.Surface, execution_time: float):
     text = f"Time: {execution_time*1000:.3f}ms"
     pg_medium_font = pg.font.SysFont('comicsans', 20)  # Font object
 
@@ -17,9 +55,9 @@ def update_screen_execution_time(screen, execution_time):
     screen.blit(text, (10, 40))  # draw the text to the screen
 
 
-def update_screen_nodes_searched(screen, n_nodes_visited):
+def update_screen_nodes_searched(screen: pg.Surface, n_nodes_visited: int):
     text = f"Nodes Searched: {n_nodes_visited}"
-    pg_medium_font = pg.font.SysFont('comicsans', 20)  # Font object
+    pg_medium_font = pg.font.SysFont("comicsans", 20)  # Font object
 
     text = pg_medium_font.render(text, True, (0, 0, 0))  # create our text
     screen.blit(text, (10, 70))  # draw the text to the screen
